17550fbe22ef36184e4b7447495f961537829e27
lancestrom <dikakewren@gmail.com>
2025-11-17 17:58:48 +0700

    mvc update 3

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -15,37 +15,9 @@ class Welcome extends CI_Controller
 		$this->load->view('tampilan_akl');
 	}
 
-	public function list_kelas_akl_x()
-	{
-		$this->load->view('tampilan_akl_x');
-	}
-	public function list_kelas_akl_xi()
-	{
-		$this->load->view('tampilan_akl_xi');
-	}
-	public function list_kelas_akl_xii()
-	{
-		$this->load->view('tampilan_akl_xii');
-	}
-
 	public function list_kelas_mplb()
 	{
 		$this->load->view('tampilan_mplbl');
-	}
-
-	public function list_kelas_mplb_x()
-	{
-		$this->load->view('tampilan_mplbl_x');
-	}
-
-	public function list_kelas_mplb_xi()
-	{
-		$this->load->view('tampilan_mplbl_xi');
-	}
-
-	public function list_kelas_mplb_xii()
-	{
-		$this->load->view('tampilan_mplbl_xii');
 	}
 
 	public function list_kelas_pm_dkv()
@@ -56,17 +28,5 @@ class Welcome extends CI_Controller
 	public function list_kelas_tjkt()
 	{
 		$this->load->view('tampilan_tjkt');
-	}
-	public function list_kelas_tjkt_x()
-	{
-		$this->load->view('tampilan_tjkt_x');
-	}
-	public function list_kelas_tjkt_xi()
-	{
-		$this->load->view('tampilan_tjkt_xi');
-	}
-	public function list_kelas_tjkt_xii()
-	{
-		$this->load->view('tampilan_tjkt_xii');
 	}
 }

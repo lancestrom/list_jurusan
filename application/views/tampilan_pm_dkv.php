49881f213c69407e28f0c900aa9e9272e0865b31
lancestrom <dikakewren@gmail.com>
2025-11-30 12:35:53 +0700

    MVC

--- application/views/tampilan_pm_dkv.php
+++ application/views/tampilan_pm_dkv.php
@@ -65,7 +65,7 @@
                 <div class="card">
                     <div class="card-body text-center">
                         <h5 class="card-title text-uppercase font-weight-bolder">X PM DKV</h5>
-                        <a class="btn btn-primary btn-sm btn-block btn-block-sm" href="http://129.212.230.155">PILIH</a>
+                        <a class="btn btn-primary btn-sm btn-block btn-block-sm" href="http://143.198.85.185">PILIH</a>
                     </div>
                 </div>
             </div>
@@ -74,7 +74,7 @@
                 <div class="card">
                     <div class="card-body text-center">
                         <h5 class="card-title text-uppercase font-weight-bolder">XI PM DKV</h5>
-                        <a class="btn btn-primary btn-sm btn-block btn-block-sm" href="http://129.212.230.155">PILIH</a>
+                        <a class="btn btn-primary btn-sm btn-block btn-block-sm" href="http://143.198.85.185">PILIH</a>
                     </div>
                 </div>
             </div>
@@ -83,7 +83,7 @@
                 <div class="card">
                     <div class="card-body text-center">
                         <h5 class="card-title text-uppercase font-weight-bolder">XII PM DKV</h5>
-                        <a class="btn btn-primary btn-sm btn-block btn-block-sm" href="http://129.212.230.155">PILIH</a>
+                        <a class="btn btn-primary btn-sm btn-block btn-block-sm" href="http://143.198.85.185">PILIH</a>
                     </div>
                 </div>
             </div>
